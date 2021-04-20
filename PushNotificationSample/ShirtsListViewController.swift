75f1d9f42f2614c78f25228108660fb32658d86f
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-04-20 22:01:13 +0900

    table

---
+++ PushNotificationSample/ShirtsListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ShirtsListViewController.swift
+//  PushNotificationSample
+//
+//  Created by 渕一真 on 2021/04/20.
+//
+
+import UIKit
+
+class ShirtsListViewController: UIViewController {
+    
+}
--- PushNotificationSample/ViewController.swift
+++ PushNotificationSample/ViewController.swift
@@ -11,9 +11,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
+    
+    @IBAction func pushWithFirebase(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Main",bundle: nil)  //手順1で説明済
+        guard let viewController = storyboard.instantiateInitialViewController() as? ShirtsListViewController else { return }  //インスタンス化したものを定数viewControllerとして定義
+        navigationController?.pushViewController(viewController, animated: true)
+    }
 
 }
 
